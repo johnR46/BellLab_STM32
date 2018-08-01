dc0771ce68053bb16df2dc03e9a5f00b05f072de
JohnR46 <Machine.The@hotmail.com>
2018-08-01 09:07:05 +0700

    normal fun SumStr

---
+++ V5/code/SumStr.c
@@ -0,0 +1,31 @@
+#include <stdio.h>  
+#include <string.h>  
+
+
+#define  notepad_y 20
+#define notepax_max 205
+
+
+char Dimen2[notepax_max][notepad_y] = {"john nonphala","surs       ak sinjlern","omg"};
+char Dimen1[notepad_y*notepax_max] = ""; //initalise so safe to pass to strcat
+    
+    
+void sumStr(){
+	
+	 int i;
+    for(i = 0; i<notepax_max;i++)
+    {
+       strcat(Dimen1,Dimen2[i]);
+    }
+    printf("%s",Dimen1);
+    
+   
+    
+}
+int main()
+{
+  
+    sumStr();
+ 
+    return 0;   
+}
